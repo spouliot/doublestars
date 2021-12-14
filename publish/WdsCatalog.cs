970e9cbb8eafe047c8830b7dc34654e559a73567
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2021-12-13 20:56:56 -0500

    Adapt for CSV file names changes

--- publish/WdsCatalog.cs
+++ publish/WdsCatalog.cs
@@ -15,35 +15,14 @@ public class WdsCatalog {
 	public WdsEntry? FindByDiscovererId (string id)
 	{
 		var key = new StringBuilder (12);
-		// discover is one to three characters
-		int i = 0;
-		key.Append (Char.ToUpperInvariant (id [i++]));
-		if (Char.IsLetter (id [1])) {
-			key.Append (Char.ToUpperInvariant (id [i++]));
-			if (Char.IsLetter (id [2]))
-				key.Append (Char.ToUpperInvariant (id [i++]));
-			else
-				key.Append (' ');
-		} else {
-			key.Append ("  ");
-		}
-		// number is one to four characters
-		var n = (int) id [i++] - (int) '0';
-		while ((i < id.Length) && Char.IsDigit (id [i])) {
-			n = n * 10 + (int) id [i++] - (int) '0';
-		}
-		key.Append ($"{n,4:D0}");
-		// components are 0 to 5 characters
-		for (int j = i; j < id.Length; j++) {
-			key.Append (Char.ToUpperInvariant (id [j]));
-		}
+		// format must be identical to the catalog, we cannot guess spaces
+		key.Append (id.ToUpperInvariant ());
+		// components are optional
 		while (key.Length < 12)
 			key.Append (' ');
 		// look up in wds catalogue
 		var key_span = key.ToString ().AsSpan ();
-		i = 0;
 		foreach (var line in wds) {
-			i++;
 			if (line.Length < 24)
 				continue;
 			if (key_span.Equals (line.AsSpan (10, 12), StringComparison.Ordinal))
